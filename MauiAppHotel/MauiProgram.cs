dbb783292587194279b379f00ffd4e77d00edc98
Otavio Henrique <otaviohenriquedasilva02@gmail.com>
2024-11-10 00:36:49 -0300

    Mudanças de desing e correção de botões.

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -16,6 +16,13 @@ namespace MauiAppHotel
                     fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
                     fonts.AddFont("Kalam-Light.ttf", "KalamLight");
                     fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("AirbnbCereal_W_Bd.otf", "CerealBd");
+                    fonts.AddFont("AirbnbCereal_W_Bk.otf", "CerealBk");
+                    fonts.AddFont("AirbnbCereal_W_Blk.otf", "CerealBlk");
+                    fonts.AddFont("AirbnbCereal_W_Lt.otf", "CerealLt");
+                    fonts.AddFont("AirbnbCereal_W_Md.otf", "CerealMd");
+                    fonts.AddFont("AirbnbCereal_W_XBd.otf", "CerealXBd");
+
                 });
 
 #if DEBUG
